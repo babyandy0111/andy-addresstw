8e2835e41f3bee22242d0818e040b8ba229bbc87
andy <babyandy0111@gmail.com>
2021-08-17 16:04:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h3>基本123</h3>
+    <Address @change="handleChange"/>
+    <p>{{ address }}</p>
+
+    <h3>default-first</h3>
+    <Address @change="handleChange" default-first/>
+
+    <h3>use props :county='新竹市' :city='東區'</h3>
+    <Address :county="address3.county" :city="address3.city" @change="handleChange"/>
+  </div>
+</template>
+
+<script>
+import Address from './components/AndyAddress.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      address: {
+        county: '臺北市',
+        city: '中正區'
+      },
+      address2: '',
+      address3: {
+        county: '新竹市',
+        city: '東區'
+      }
+    }
+  },
+  components: {
+    Address
+  },
+  methods: {
+    handleChange(data) {
+      this.address = data
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Address from './AndyAddress'
+
+Address.install = function (Vue) {
+    Vue.component('vue-address', Address)
+}
+
+export default Address
